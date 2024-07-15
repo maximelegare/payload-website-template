8817e51fcc9c39e0195dd56cbd790906a289f9a5
maximelegare <mleg1234@protonmail.com>
2024-07-15 17:59:33 -0400

    fix(middleware): fix access to admin in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,16 @@
 import { i18nRouter } from 'next-i18n-router'
-import  i18nConfig  from '../i18nConfig'
-import { NextRequest } from 'next/server'
+import i18nConfig from '../i18nConfig'
+import { NextRequest, NextResponse } from 'next/server'
 
-export function middleware(request:NextRequest) {
-  return i18nRouter(request, i18nConfig)
+export function middleware(req: NextRequest) {
+  if (req.url.includes('admin')) {
+    return NextResponse.redirect(new URL('/admin', req.url))
+  }
+
+  return i18nRouter(req, i18nConfig)
 }
 
 // only applies this middleware to files in the app directory
 export const config = {
-  matcher: '/((?!api|static|.*\\..*|_next).*)',
+  matcher: '/((?!api|static|.*\\..*|_next|admin).*)',
 }
