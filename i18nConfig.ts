735951bfdb879c23371b24d815f3561c5b1de9df
maximelegare <mleg1234@protonmail.com>
2024-07-14 21:55:01 -0400

    init(init & add internationalization):

---
+++ i18nConfig.ts
@@ -0,0 +1,9 @@
+import { defaultLocale, locales } from './src/locales/locales'
+import { Config } from 'next-i18n-router/dist/types'
+
+export const i18nConfig: Config = {
+  defaultLocale,
+  locales: locales.map((l) => l.locale),
+}
+
+export type Locale = (typeof i18nConfig)['locales'][number]
---
+++ src/locales/locales.ts
@@ -0,0 +1,8 @@
+const locales = [
+  { locale: 'fr', labels: { fr: 'Français', en: 'French' }, flag:"🇫🇷" },
+  { locale: 'en', labels: { fr: 'Anglais', en: 'English' }, flag:"🇬🇧"},
+]
+
+const defaultLocale = 'fr'
+
+export { locales, defaultLocale }
