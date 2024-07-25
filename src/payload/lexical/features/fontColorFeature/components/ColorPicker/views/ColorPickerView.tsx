6e6641d3f03d4de3fd9590654f68e248387dfb31
maximelegare <mleg1234@protonmail.com>
2024-07-24 22:38:51 -0400

    fix(color picker): HEX codes works

--- src/payload/lexical/features/fontColorFeature/components/ColorPicker/views/ColorPickerView.tsx
+++ src/payload/lexical/features/fontColorFeature/components/ColorPicker/views/ColorPickerView.tsx
@@ -55,10 +55,9 @@ export const ColorPickerView = ({ fontColor, onFontColorChange, onApplyStyles }:
       ...prev,
       [name]: { ...prev[name], [subName]: name === 'hex' ? value : parseInt(value) },
     }))
-
     let HEX: string
     if (name === 'hex') {
-      HEX = color
+      HEX = value
     } else if (name === 'hsl') {
       HEX = translateColor({ H: inputs.hsl.h, S: inputs.hsl.s, L: inputs.hsl.l }, 'HEX')
     } else if (name === 'rgb') {
@@ -67,142 +66,147 @@ export const ColorPickerView = ({ fontColor, onFontColorChange, onApplyStyles }:
     }
 
     setColor(HEX)
+    console.log(HEX)
 
     onFontColorChange(color)
   }
 
   return (
-    <>
-      {
-        <div className="flex gap-4">
-          <div className="flex flex-col gap-3">
-            <HexColorPicker
-              id="colorpicker"
-              color={color}
-              onChange={(color) => {
-                handleFontColorChange(color)
-              }}
+    <div className="flex flex-col">
+      <div className="flex gap-4">
+        <div className="flex flex-col gap-3">
+          <HexColorPicker
+            id="colorpicker"
+            color={color}
+            onChange={(color) => {
+              handleFontColorChange(color)
+            }}
+          />
+          <div className="flex gap-2 items-center w-full ">
+            <div className="flex-grow w-full"></div>
+            <Label htmlFor="hex-color-picker-input">HEX</Label>
+            <Input
+              value={cleanInput(inputs.hex.value)}
+              name="hex.value"
+              id="hex-color-picker-input"
+              beforeIcon={<span className="text-lg">#</span>}
+              type="text"
+              maxLength={7}
+              className="tracking-[0.2rem] text-base w-[120px]"
+              onChange={handleInputChange}
+              onBlur={(e) => cleanInput(e.target.value)}
             />
-            <div className="flex gap-2 items-center w-full ">
-              <div className="flex-grow w-full"></div>
-              <Label htmlFor="hex-color-picker-input">HEX</Label>
+          </div>
+        </div>
+        <div className="flex flex-col gap-6">
+          <div className="flex flex-col gap-2">
+            <div className="flex gap-2 items-center w-full">
+              <Label htmlFor="hex-color-picker-input" className="w-3">
+                H
+              </Label>
               <Input
-                value={cleanInput(inputs.hex.value)}
-                name="hex.value"
+                value={inputs.hsl.h}
                 id="hex-color-picker-input"
-                beforeIcon={<span className="text-lg">#</span>}
-                type="text"
+                name="hsl.h"
+                type="number"
+                min={0}
+                max={360}
                 maxLength={7}
-                className="tracking-[0.2rem] text-base w-[120px]"
+                className="tracking-[0.2rem] text-base w-24"
                 onChange={handleInputChange}
-                onBlur={(e) => cleanInput(e.target.value)}
+              />
+            </div>
+            <div className="flex gap-2 items-center w-full">
+              <Label htmlFor="hex-color-picker-input" className="w-3">
+                S
+              </Label>
+              <Input
+                id="hex-color-picker-input"
+                value={inputs.hsl.s}
+                name="hsl.s"
+                type="number"
+                max={100}
+                min={0}
+                maxLength={7}
+                className="tracking-[0.2rem] text-base w-24"
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="flex gap-2 items-center w-full">
+              <Label htmlFor="hex-color-picker-input" className="w-3">
+                L
+              </Label>
+              <Input
+                value={inputs.hsl.l}
+                id="hex-color-picker-input"
+                name="hsl.l"
+                type="number"
+                min={0}
+                max={100}
+                maxLength={7}
+                className="tracking-[0.2rem] text-base w-24"
+                onChange={handleInputChange}
               />
             </div>
           </div>
-          <div className="flex flex-col gap-6">
-            <div className="flex flex-col gap-2">
-              <div className="flex gap-2 items-center w-full">
-                <Label htmlFor="hex-color-picker-input" className="w-3">
-                  H
-                </Label>
-                <Input
-                  value={inputs.hsl.h}
-                  id="hex-color-picker-input"
-                  name="hsl.h"
-                  type="number"
-                  min={0}
-                  max={360}
-                  maxLength={7}
-                  className="tracking-[0.2rem] text-base w-24"
-                  onChange={handleInputChange}
-                />
-              </div>
-              <div className="flex gap-2 items-center w-full">
-                <Label htmlFor="hex-color-picker-input" className="w-3">
-                  S
-                </Label>
-                <Input
-                  id="hex-color-picker-input"
-                  value={inputs.hsl.s}
-                  name="hsl.s"
-                  type="number"
-                  max={100}
-                  min={0}
-                  maxLength={7}
-                  className="tracking-[0.2rem] text-base w-24"
-                  onChange={handleInputChange}
-                />
-              </div>
-              <div className="flex gap-2 items-center w-full">
-                <Label htmlFor="hex-color-picker-input" className="w-3">
-                  L
-                </Label>
-                <Input
-                  value={inputs.hsl.l}
-                  id="hex-color-picker-input"
-                  name="hsl.l"
-                  type="number"
-                  min={0}
-                  max={100}
-                  maxLength={7}
-                  className="tracking-[0.2rem] text-base w-24"
-                  onChange={handleInputChange}
-                />
-              </div>
+          {/* <div className="flex flex-col gap-2">
+            <div className="flex gap-2 items-center w-full">
+              <Label htmlFor="hex-color-picker-input" className="w-3">
+                R
+              </Label>
+              <Input
+                value={inputs.rgb.r}
+                id="hex-color-picker-input"
+                name="rgb.r"
+                type="number"
+                min={0}
+                max={360}
+                maxLength={7}
+                className="tracking-[0.2rem] text-base w-24"
+                onChange={handleInputChange}
+              />
             </div>
-            <div className="flex flex-col gap-2">
-              <div className="flex gap-2 items-center w-full">
-                <Label htmlFor="hex-color-picker-input" className="w-3">
-                  R
-                </Label>
-                <Input
-                  value={inputs.rgb.r}
-                  id="hex-color-picker-input"
-                  name="rgb.r"
-                  type="number"
-                  min={0}
-                  max={360}
-                  maxLength={7}
-                  className="tracking-[0.2rem] text-base w-24"
-                  onChange={handleInputChange}
-                />
-              </div>
-              <div className="flex gap-2 items-center w-full">
-                <Label htmlFor="hex-color-picker-input" className="w-3">
-                  G
-                </Label>
-                <Input
-                  id="hex-color-picker-input"
-                  value={inputs.rgb.g}
-                  name="rgb.g"
-                  type="number"
-                  max={100}
-                  min={0}
-                  maxLength={7}
-                  className="tracking-[0.2rem] text-base w-24"
-                  onChange={handleInputChange}
-                />
-              </div>
-              <div className="flex gap-2 items-center w-full">
-                <Label htmlFor="hex-color-picker-input" className="w-3">
-                  B
-                </Label>
-                <Input
-                  value={inputs.rgb.b}
-                  id="hex-color-picker-input"
-                  name="rgb.b"
-                  type="number"
-                  min={0}
-                  max={100}
-                  maxLength={7}
-                  className="tracking-[0.2rem] text-base w-24"
-                  onChange={handleInputChange}
-                />
-              </div>
+            <div className="flex gap-2 items-center w-full">
+              <Label htmlFor="hex-color-picker-input" className="w-3">
+                G
+              </Label>
+              <Input
+                id="hex-color-picker-input"
+                value={inputs.rgb.g}
+                name="rgb.g"
+                type="number"
+                max={100}
+                min={0}
+                maxLength={7}
+                className="tracking-[0.2rem] text-base w-24"
+                onChange={handleInputChange}
+              />
             </div>
-          </div>
+            <div className="flex gap-2 items-center w-full">
+              <Label htmlFor="hex-color-picker-input" className="w-3">
+                B
+              </Label>
+              <Input
+                value={inputs.rgb.b}
+                id="hex-color-picker-input"
+                name="rgb.b"
+                type="number"
+                min={0}
+                max={100}
+                maxLength={7}
+                className="tracking-[0.2rem] text-base w-24"
+                onChange={handleInputChange}
+              />
+            </div>
+          </div> */}
         </div>
-      }
-    </>
+      </div>
+      <div className="flex w-full justify-between">
+        <div></div>
+        <Button onClick={onApplyStyles} buttonStyle="primary" size="small" className="w-24">
+          Apply
+        </Button>
+      </div>
+    </div>
   )
 }
